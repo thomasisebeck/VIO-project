652971af501900b7270294a3e05f69a89ab67413
Thomas Isebeck <thomasisebeck@gmail.com>
2022-10-18 21:57:00 +0200

    Added title

--- Theme 4/D2/vio/src/components/ParkScene/ParkScene.js
+++ Theme 4/D2/vio/src/components/ParkScene/ParkScene.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './ParkScene.css';
 import ChangingImage from '../ChangingImage/ChangingImage';
 
-export class ParkScene extends React.Component {
+export class  ParkScene extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      source: 'images_website/dialogue/1.png', 
+      source: 'images_website/splash/dialogue/1.png', 
       currentImage: 1, 
-      dialogueShown: false, 
+      dialogueShown: true, 
       MAX_IMAGES: 7, 
       seeTrailer: false, 
       goToComic: false
@@ -17,13 +17,14 @@ export class ParkScene extends React.Component {
 
     setInterval(() => {
         this.loopDialogue();
-    }, 2000);
+    }, 500);
 
 
 
   }
 
   loopDialogue() {    
+
     if (this.state.currentImage === this.state.MAX_IMAGES) {
 
       if (this.state.dialogueShown)
@@ -36,7 +37,7 @@ export class ParkScene extends React.Component {
     else 
       this.setState({currentImage: this.state.currentImage + 1 })
 
-      this.setState({ source: 'images_website/dialogue/' + this.state.currentImage + '.png' })
+      this.setState({ source: 'images_website/splash/dialogue/' + this.state.currentImage + '.png' })
     
   }
 
@@ -76,38 +77,36 @@ export class ParkScene extends React.Component {
 
         <div className="Content">
 
-          <img src="images_website/background.svg" alt=""/>
+          <img src="images_website/splash/background.svg" alt=""/>
 
           <div id="cloudsWrapper"> 
-            <img id="clouds" src="images_website/clouds.gif" alt=""/>
+            <img id="clouds" src="images_website/splash/clouds.gif" alt=""/>
           </div> 
           
           <div className='MCSitting' onMouseOver={() => this.displayTrailerDialogue()} onMouseLeave={() => this.undisplayTrailerDialogue()}>
-            <ChangingImage movingimage='images_website/MC_hover.gif' staticimage='images_website/MC_idle.gif' />
+            <ChangingImage movingimage='images_website/splash/MC_hover.gif' staticimage='images_website/splash/MC_idle.gif' />
           </div>
 
           <div className='WomanSitting' onMouseOver={() => this.displayComicDialogue()} onMouseLeave={() => this.undisplayComicDialogue()}>
-            <ChangingImage movingimage='images_website/woman_hover.gif' staticimage='images_website/woman_idle.gif' />
+            <ChangingImage movingimage='images_website/splash/woman_hover.gif' staticimage='images_website/splash/woman_idle.gif' />
           </div>
 
-          {this.state.dialogueShown && this.state.source !== 'images_website/dialogue/' + this.state.MAX_IMAGES + '.png' &&
+          {this.state.dialogueShown && this.state.source !== 'images_website/splash/dialogue/' + this.state.MAX_IMAGES + '.png' &&
             <img id="dialogueImage" src={this.state.source} alt=""/>
           }
 
           {this.state.seeTrailer &&
-            <img id="seeTrailer" src="images_website/seeTrailer.png" alt=""/>
+            <img id="seeTrailer" src="images_website/splash/seeTrailer.png" alt=""/>
           }
 
           {this.state.goToComic &&
-            <img id="goToComic" src="images_website/goToComic.png" alt=""/>
+            <img id="goToComic" src="images_website/splash/goToComic.png" alt=""/>
           }
         </div>
 
         <div className="Title">
-          You <br/>
-          read <br/>
-          my <br/>
-          mind!
+          <img src='images_website/splash/bottomTitleCard.png'/>
+          <img src='images_website/splash/topTitleCard.png'/>
         </div>
 
       </div>
