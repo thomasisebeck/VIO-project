e77fa3c680e7bace6dd57ea3eedc7c10304bdad0
Thomas Isebeck <thomasisebeck@gmail.com>
2022-11-03 01:11:15 +0200

    embed works with sound but no smoke

---
+++ Theme 4/D2/vio/src/components/Stage/Stage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Stage.css';
+
+const Stage = () => (
+  <div className="Stage">
+    Stage Component
+  </div>
+);
+
+Stage.propTypes = {};
+
+Stage.defaultProps = {};
+
+export default Stage;
