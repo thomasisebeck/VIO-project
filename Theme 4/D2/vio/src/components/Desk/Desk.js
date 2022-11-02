86f8e26f24fac8b666e838f432a9638b0090a9b8
ThomasIsebeck <thomasisebeck@gmail.com>
2022-11-02 09:47:42 +0100

    hat now falls of table

--- Theme 4/D2/vio/src/components/Desk/Desk.js
+++ Theme 4/D2/vio/src/components/Desk/Desk.js
@@ -8,7 +8,9 @@ export class  Desk extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bookOpen: false
+      bookOpen: false, 
+      hat: 'images_website/desk/hat.png', 
+      hatClass: 'hat'
     }
   }
 
@@ -23,13 +25,22 @@ export class  Desk extends React.Component {
     })
   }
 
+  hatFalling() {
+    this.setState({
+      hat: 'images_website/desk/hat.gif',
+      hatClass: 'hat fade'
+    })
+
+  }
+
   render() {
     return (
       <div className="Desk">
           <div className='innerDesk'>
             <img className='desk' src="images_website/desk/desk.png" />
             <img className="comic" onClick={() => { this.showComic(); }} src="images_website/desk/comic.png" />
-            <img className='hat' src='images_website/desk/hat.png' />
+            <img className='key' src='images_website/desk/key.png' />
+            <img className={this.state.hatClass} onClick={() => { this.hatFalling(); }} src={this.state.hat} />
             <img className='letter' src='images_website/desk/letter.png' />
           </div>
 
