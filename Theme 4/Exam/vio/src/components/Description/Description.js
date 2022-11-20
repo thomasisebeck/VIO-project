861d79910cadf34e00e4475f0275a1a4cedfdf7e
Thomas Isebeck <thomasisebeck@gmail.com>
2022-11-20 19:10:10 +0200

    comic and play have click events

--- Theme 4/Exam/vio/src/components/Description/Description.js
+++ Theme 4/Exam/vio/src/components/Description/Description.js
@@ -9,8 +9,20 @@ export class  Description extends React.Component {
     this.state = {
       img: 'images_website/descriptions/cactusRight.gif',
       heading: 'The Comic', 
-      para: 'Things about the comic...'
+      para: 'Things about the comic...', 
+      play: false,
+      comic: false
     }
+  }
+
+  changePara = (trailer) => {
+    if (this.state.img == 'images_website/descriptions/cactusRight.gif')
+      this.setState({ img: 'images_website/descriptions/cactusLeft.gif'})
+    else 
+    this.setState({ img: 'images_website/descriptions/cactusRight.gif' })
+
+
+    console.log('comic para');
   }
 
   render() {
@@ -18,8 +30,18 @@ export class  Description extends React.Component {
       <div className="Description">
         <div id='descriptions'>
           <div className='imageContainer'>
-            <img src='images_website/descriptions/playButton.png' />
-            <img src='images_website/descriptions/comic.png' />
+            <img 
+              onMouseOver={() => this.setState({ play: true})}
+              onMouseOut={() => this.setState({ play: false})}
+              onClick={() => this.changePara(true) }
+              className={this.state.play ? 'pulse' : ''}
+              src='images_website/descriptions/playButton.png' />
+            <img 
+              onMouseOver={() => this.setState({ comic: true})}
+              onMouseOut={() => this.setState({ comic: false})}
+              onClick={() => this.changePara(false) }
+              className={this.state.comic ? 'pulse' : ''}
+              src='images_website/descriptions/comic.png' />
           </div>
           <div className='descrContainer'>
             <div>
